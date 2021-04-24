fc67072a27e9019e0b1dae6f45bf89a46efe9913
yassinemor <yassinemortazik132@gmail.com>
2021-04-24 20:29:10 +0000

    headre,footer and login page done

---
+++ resources/views/footer.blade.php
@@ -0,0 +1,8 @@
+ <br>
+ <br>
+ <br>
+ <br>
+ <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
+    © 2020 Copyright:
+    <a class="text-white" href=""> shoft</a>
+  </div>
